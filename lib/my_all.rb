c937da132fc208a0f23fab03cfca796c97ae5d13
Tymeckia Kendall <tykend@gmail.com>
2019-03-05 04:12:03 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,15 +1,14 @@
 require 'pry'
 
 def my_all?(collection)
-  i=0
-  block_return_values=[]
+  i = 0
+  block_return_values = []
   while i < collection.length
-  i += 1
-  block_return_values << yield(collection[i])
-    i += 1
+    block_return_values << yield(collection[i])
+    i = i + 1
   end
-  
-    if block_return_values.include?(false)
+ 
+  if block_return_values.include?(false)
     false
   else
     true
